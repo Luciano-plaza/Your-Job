22a19c2a4db2bb3e3bda587933a51a8bb594d545
Tomshaster <tomascartagena00@gmail.com>
2022-06-08 11:53:37 -0300

    instalado bootstrap/ creada la página de onboarding
    instalé bootstrap y lo usé para crear la página de onboarding

--- Client/src/Pages/Onboarding/Onboarding.jsx
+++ Client/src/Pages/Onboarding/Onboarding.jsx
@@ -1,3 +1,20 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 export default function Onboarding() {
-  return <div>onboarding</div>;
+  return (
+    <Card className="text-center">
+      <Card.Header>Hello New User!</Card.Header>
+      <Card.Body>
+        <Card.Title>Please choose one of the options below</Card.Title>
+        <Card.Text>
+          We would like to know what use you will be giving this page
+        </Card.Text>
+        <Button variant="primary">Employee</Button>
+        <br />
+        <br />
+        <Button variant="primary">Company</Button>
+      </Card.Body>
+    </Card>
+  );
 }
